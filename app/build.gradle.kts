98d8c8bf39a0fbe97062e1b32cc73bd5c21d43b0
NolifekNTB <kacperkotlowskikk1@gmail.com>
2024-06-07 11:23:26 +0200

    Add Dagger-Hilt gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     id("kotlin-kapt")
-    //id("dagger.hilt.android.plugin")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -54,7 +54,7 @@ android {
 dependencies {
     //Hilt
     implementation ("com.google.dagger:hilt-android:2.50")
-    //kapt ("com.google.dagger:hilt-android-compiler:2.40.5")
+    kapt ("com.google.dagger:hilt-android-compiler:2.40.5")
 
     //Gson
     implementation ("com.google.code.gson:gson:2.11.0")
