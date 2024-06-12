3cba955bd6e8c8131b4c81c9920340ff2a622d08
NolifekNTB <kacperkotlowskikk1@gmail.com>
2024-06-12 08:59:12 +0200

    Improve UI of the ReceiptListScreen

--- app/src/main/java/com/example/przyjeciamagazyn/Receipts/presentation/screens/Receipt/ReceiptListScreen.kt
+++ app/src/main/java/com/example/przyjeciamagazyn/Receipts/presentation/screens/Receipt/ReceiptListScreen.kt
@@ -1,29 +1,57 @@
 package com.example.przyjeciamagazyn.Receipts.presentation.screens.Receipt
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.przyjeciamagazyn.Contractors.data.model.Contractor
 import com.example.przyjeciamagazyn.Core.presentation.Navigation.AddScreens
 import com.example.przyjeciamagazyn.Core.presentation.BackButton
 import com.example.przyjeciamagazyn.Core.presentation.Navigation.Screen
+import com.example.przyjeciamagazyn.R
 import com.example.przyjeciamagazyn.Receipts.data.model.ReceiptDocument
 import com.example.przyjeciamagazyn.Receipts.presentation.ReceiptViewModel
+import java.util.Locale
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ReceiptListScreen(receiptViewModel: ReceiptViewModel, onNavigate: (String) -> Unit, ) {
     val documents = receiptViewModel.receiptDocuments.collectAsState(emptyList()).value
@@ -33,38 +61,81 @@ fun ReceiptListScreen(receiptViewModel: ReceiptViewModel, onNavigate: (String) -
         receiptViewModel.getALlReceipts()
     }
 
-    Column(Modifier.fillMaxSize()) {
-        BackButton("Receipt List",Modifier.padding(top = 20.dp, start = 15.dp)) {route -> onNavigate(route)}
-        Spacer(modifier = Modifier.height(16.dp))
-        LazyColumn(modifier = Modifier.weight(1f)) {
-            items(documents) { ReceiptDocument ->
-                DocumentRow(ReceiptDocument) { document ->
-                    selectedDocument.value = document
-                    onNavigate(Screen.DocumentDetailScreen.route)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Receipt List") },
+                navigationIcon = {
+                    IconButton(onClick = { onNavigate("back_route") }) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { onNavigate(AddScreens.AddDocumentSheet.route) }) {
+                Icon(Icons.Default.Add, contentDescription = "Add Document")
+            }
+        },
+        content = { paddingValues ->
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)) {
+                    LazyColumn(modifier = Modifier.weight(1f)) {
+                      items(documents) { receiptDocument ->
+                            DocumentRow(receiptDocument) { document ->
+                                selectedDocument.value = document
+                               onNavigate(Screen.DocumentDetailScreen.route)
+                            }
+                    }
                 }
             }
         }
-        Button(
-            onClick = { onNavigate(AddScreens.AddDocumentSheet.route) },
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .fillMaxWidth()
-                .padding(16.dp))
-        {
-            Text(text = "Dodaj dokument")
+    )
+}
+
+@Composable
+fun DocumentRow(receiptDocument: ReceiptDocument, onDocumentClick: (ReceiptDocument) -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable { onDocumentClick(receiptDocument) },
+        elevation = CardDefaults.elevatedCardElevation(4.dp)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = "Date: ${receiptDocument.date}", style = MaterialTheme.typography.titleMedium)
+            Text(text = "Symbol: ${receiptDocument.symbol}", style = MaterialTheme.typography.titleMedium)
+            Text(text = "Contractors:", style = MaterialTheme.typography.titleMedium)
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Column {
+                receiptDocument.contractors.forEach { contractor ->
+                    ContractorRow(contractor)
+                }
+            }
         }
     }
 }
 
 @Composable
-fun DocumentRow(ReceiptDocument: ReceiptDocument, whichDocument: (ReceiptDocument) -> Unit) {
-    Column(
+fun ContractorRow(contractor: Contractor) {
+    Row(
         modifier = Modifier
-            .fillMaxSize()
-            .clickable { whichDocument(ReceiptDocument) }
+            .fillMaxWidth()
+            .padding(vertical = 4.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(text = "Data: ${ReceiptDocument.date}", fontSize = 18.sp)
-        Text(text = "Symbol: ${ReceiptDocument.symbol}", fontSize = 18.sp)
-        Text(text = "Kontrahent: ${ReceiptDocument.contractors}", fontSize = 18.sp)
+        Text(
+            text = contractor.symbol,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.width(100.dp)
+        )
+        Text(
+            text = contractor.name,
+            style = MaterialTheme.typography.bodyLarge
+        )
     }
 }
