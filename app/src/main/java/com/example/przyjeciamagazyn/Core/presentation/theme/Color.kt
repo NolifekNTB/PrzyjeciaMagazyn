d1d2a2d121b343e888473565a7b430396dee8433
NolifekNTB <kacperkotlowskikk1@gmail.com>
2024-06-12 08:39:08 +0200

    Before improve UI

--- app/src/main/java/com/example/przyjeciamagazyn/Core/presentation/theme/Color.kt
+++ app/src/main/java/com/example/przyjeciamagazyn/Core/presentation/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val LightPrimary = Color(0xFF6200EE)
+val LightPrimaryVariant = Color(0xFF3700B3)
+val LightSecondary = Color(0xFF03DAC6)
+
+val Red = Color(0xFFFF0000)
+val Blue = Color(0xFF0000FF)
+
+val DarkPrimary = Color(0xFFBB86FC)
+val DarkPrimaryVariant = Color(0xFF3700B3)
+val DarkSecondary = Color(0xFF03DAC6)
